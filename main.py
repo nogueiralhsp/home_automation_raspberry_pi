b7d1442b22aae7bba6bfa54aba41cf9359cbf08f
Henrique Nogueira <nogueiralhsp@gmail.com>
2021-11-29 16:45:34 +0000

    all lights off starting

--- main.py
+++ main.py
@@ -72,10 +72,10 @@ serialExists = False
 apiRefreshTime = 2000  # time to call api for refreshing / call api
 
 
-# # used to run app through SSH
-# if os.environ.get('DISPLAY', '') == '':
-#     # print('no display found. Using :0.0') #commented out when don't want to log in terminal
-#     os.environ.__setitem__('DISPLAY', ':0.0')
+# used to run app through SSH
+if os.environ.get('DISPLAY', '') == '':
+    # print('no display found. Using :0.0') #commented out when don't want to log in terminal
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 
 # checking if there Arduino is connected and USB port is available
